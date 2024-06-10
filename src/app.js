ca054b89019cdaf26311d88b55943a016a8c4134
Gustavo Mendes <gusm217@gmail.com>
2024-06-10 16:56:17 -0300

    feat: add getUserById route

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const createUsersRoute = require('./routes/createUsers');
 const listUsers = require('./routes/listUsers');
+const getUserById = require('./routes/getUserById');
 
 const app = express();
 app.use(express.json());
@@ -9,6 +10,7 @@ let users = [];
 
 app.use('/', createUsersRoute(users));
 app.use('/', listUsers(users));
+app.use('/', getUserById(users));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
---
+++ src/routes/getUserById.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const router = express.Router()
+
+const getUserById = (users) => {
+	router.get('/users/:id', (req, res) => {
+	const { id } = req.params;
+	const user = users.find(u => u.id === id);
+	if (!user) {
+			return res.status(404).json({ error: 'User not found' });
+	}
+
+	res.status(200).json(user);
+	})
+
+	return router;
+}
+
+module.exports = getUserById;
