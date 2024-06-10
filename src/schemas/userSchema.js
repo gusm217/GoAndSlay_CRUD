91db76a8bdb23f2131653db3805a291f17db3529
Gustavo Mendes <gusm217@gmail.com>
2024-06-10 20:27:07 -0300

    fix: add strict validation

--- src/schemas/userSchema.js
+++ src/schemas/userSchema.js
@@ -5,6 +5,6 @@ const userSchema = Joi.object({
   name: Joi.string().min(3).max(30).required(),
   email: Joi.string().email().required(),
   age: Joi.number().integer().positive().required()
-});
+}).strict();
 
 module.exports = userSchema;
