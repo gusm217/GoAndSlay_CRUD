1899e424d83df94c0afe16010f258dcf0d39627d
Gustavo Mendes <gusm217@gmail.com>
2024-06-10 17:10:11 -0300

    feat: add updateUser route

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const createUsersRoute = require('./routes/createUsers');
 const listUsers = require('./routes/listUsers');
 const getUserById = require('./routes/getUserById');
+const updateUser = require('./routes/updateUser');
 
 const app = express();
 app.use(express.json());
@@ -11,6 +12,7 @@ let users = [];
 app.use('/', createUsersRoute(users));
 app.use('/', listUsers(users));
 app.use('/', getUserById(users));
+app.use('/', updateUser(users));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
---
+++ src/routes/updateUser.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const userSchema = require('../schemas/userSchema')
+
+const router = express.Router()
+
+const updateUser = (users) => {
+	router.put('/users/:id', (req, res) => {
+		const { id } = req.params;
+		const { name, email, age } = req.body;
+		const userIndex = users.findIndex(u => u.id === id);
+		if (userIndex === -1) {
+				return res.status(404).json({ error: 'User not found' });
+		}
+		const updatedUser = { id, name, email, age };
+
+		const { error } = userSchema.validate(updatedUser);
+		if (error) {
+			return res.status(400).json({ message: error.details[0].message });
+		}
+
+		users[userIndex] = updatedUser;
+		res.status(200).json(updatedUser);
+	})
+
+	return router;
+}
+
+module.exports = updateUser;
